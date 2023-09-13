75212248a1dc67b9f026957a330216e122c871a6
supunj506 <supunj506@gmail.com>
2023-09-13 12:47:20 +0530

    test01

---
+++ src/HelloServlet.java
@@ -0,0 +1,10 @@
+/*
+ * Author  : supun@mj
+ * Project : Hello_Java_EE
+ * Date    : 9/12/2023 Tuesday
+ * Time    : 11:16 PM
+ * For GDSE course of IJSE institute.
+ */
+
+public class HelloServlet extends HttpServlet{
+}
